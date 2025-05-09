c137ae4361fea3e5df71794dc2ed592067e16892
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-05-09 11:19:20 +0200

    Demo App class added

--- src/main/java/com/tree/App.java
+++ src/main/java/com/tree/App.java
@@ -1,13 +1,20 @@
 package com.tree;
 
 /**
- * Hello world!
+ * Demo BST
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        DirectoryTree dt = new DirectoryTree();
+        dt.add("Downloads");
+        dt.add("Music");
+        dt.add("Documents");
+        dt.add("Photos");
+
+        System.out.println("Directory listing:");
+        dt.listInOrder().forEach(System.out :: println);
+
+
     }
 }
